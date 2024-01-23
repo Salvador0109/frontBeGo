e068bee0e25f9f34383684cf016461d0b5ecd0a7
Salvador0109 <salvador_0109@hotmail.com>
2024-01-23 16:27:06 -0600

    first commit

---
+++ src/app/home/cargo-orders/cargo-details/cargo-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargo-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './cargo-details.component.html',
+  styleUrl: './cargo-details.component.scss'
+})
+export class CargoDetailsComponent {
+
+}
---
+++ src/app/home/cargo-orders/cargo-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargo-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './cargo-orders.component.html',
+  styleUrl: './cargo-orders.component.scss'
+})
+export class CargoOrdersComponent {
+
+}
