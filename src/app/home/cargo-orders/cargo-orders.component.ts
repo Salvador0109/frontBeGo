ae8332449325acf367c5ea49686d212ece7ee715
Salvador0109 <salvador_0109@hotmail.com>
2024-01-24 01:37:56 -0600

    Desarrollo de layout para Cargo Orders

--- src/app/home/cargo-orders/cargo-orders.component.ts
+++ src/app/home/cargo-orders/cargo-orders.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../components/header/header.component';
+import { OrderComponent } from '../../../components/order/order.component';
 
 @Component({
   selector: 'app-cargo-orders',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, OrderComponent],
   templateUrl: './cargo-orders.component.html',
   styleUrl: './cargo-orders.component.scss'
 })
 export class CargoOrdersComponent {
-
+  title = "Cargo Orders";
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ComponentsModule } from '../../components/components.module';
+import { HeaderComponent } from '../../components/header/header.component';
+import { CargoOrdersComponent } from './cargo-orders/cargo-orders.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CargoOrdersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
